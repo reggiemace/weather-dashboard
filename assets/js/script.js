dd8807da6f60fc4267506572345c16d65c90f5da
Reeggie Mace <reginaldmace36@gmail.com>
2021-03-31 17:48:43 -0400

    updated READ.me. and included best effort functionality to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,10 @@ var currentCityArray = JSON.parse(localStorage.getItem("cities")) || [];
 var forecastSection = document.querySelector("#forecast-div");
 var searchHistory = document.querySelector("#searchHistory");
 var clearButton = document.querySelector("#clearBtn");
-var searchedCity = document.querySelector("#searched-city");
+var searchedCity = document.querySelector(".searched-city");
 var foreCastHeader = document.querySelector("#current-city-name");
 
+//Hide Forecast Section on Page Load
 window.onload = function () {
   forecastSection.style.display = "none";
   foreCastHeader.style.display = "none";
@@ -23,13 +24,12 @@ window.onload = function () {
 // Search city
 function searchCity() {
   cityName = document.getElementById("search-field").value;
-  //console.log(cityName);
   getCityWeatherInfo(cityName);
   getForecast(cityName);
   currentCityName;
 }
 
-// Fetch weather information from API
+// Get Current Weather Data API
 function getCityWeatherInfo(cityName) {
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -46,34 +46,30 @@ function getCityWeatherInfo(cityName) {
       console.log(data);
       var currentTemp = data.main.temp;
       var currentCity = data.name;
-      console.log(currentCity);
       currentHumidity = data.main.humidity;
-      currentWindSpeed = data.wind.speed;
       currentWindSpeed = data.wind.speed;
       icon = data.weather[0].icon;
       iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      console.log(iconUrl);
-      console.log("icon");
-      console.log("The current UV Index " + currentWindSpeed);
       var currentDay = data.dt;
       currentDay = moment().format("L");
-      console.log("icon " + icon);
+      currentCityName.innerHTML = currentCity;
+      currentCityTemp.innerHTML = currentTemp + "&deg" + "F";
+      currentCityHumidity.innerHTML = currentHumidity + "%";
+      currentCityWindSpeed.innerHTML = currentWindSpeed;
+      currentDay.innerHTML = currentDay;
+      iconCurrent.innerHTML = "<img src=" + iconUrl + ">";
+      currentCityUV.innerHTML = "coming soon!!!!";
 
       // Set City Name to city Array
       currentCityArray.push(currentCity);
       localStorage.setItem("City", currentCity);
-      localStorage.setItem("Temperature", currentTemp);
-      localStorage.setItem("Humidity", currentHumidity);
-      localStorage.setItem("Wind Speed", currentWindSpeed);
-      localStorage.setItem("Current Day", currentDay);
-      localStorage.setItem("Icon", icon);
       localStorage.setItem("cities", JSON.stringify(currentCityArray));
-      iconCurrent.innerHTML = "<img src=" + iconUrl + ">";
     });
-  displayWeatherInfo();
+  // Show forecast section after a city has been searched
   forecastSection.style.display = "block";
   foreCastHeader.style.display = "block";
 }
+// Get 5 Day/ 3 hour forecast
 function getForecast(cityName) {
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -95,12 +91,8 @@ function getForecast(cityName) {
         futureDayIcon = data.list[i].weather[0].icon;
         futureDayUrl =
           "http://openweathermap.org/img/wn/" + futureDayIcon + "@2x.png";
-        console.log("The day is " + futureDay);
-        console.log("The Temp is " + futureTemp);
-        console.log("The Humidity is " + futureHumid);
-        console.log("Future Icon" + futureDayUrl);
-        length,
-          $(`
+
+        $(`
           <div class="custom-card">
             <p>${futureDay}</p>
             <p><img src="${futureDayUrl}"></p>
@@ -109,22 +101,23 @@ function getForecast(cityName) {
           </div>
         `).appendTo("#forecast-div");
       }
-      displayWeatherInfo();
+
+      //displayWeatherInfo();
     });
 }
-// Display Weather Back to User
-function displayWeatherInfo() {
+
+// This function is not needed
+/* function displayWeatherInfo() {
   currentCityName.innerHTML = localStorage.getItem("City");
   currentCityTemp.innerHTML =
-    localStorage.getItem("Temperature") + "&deg" + "F";
+  localStorage.getItem("Temperature") + "&deg" + "F";
   currentCityHumidity.innerHTML = localStorage.getItem("Humidity") + "%";
   currentCityWindSpeed.innerHTML = localStorage.getItem("Wind Speed");
   currentDay.innerHTML = localStorage.getItem("Current Day");
-  //iconVal = localStorage.getItem("Icon");
-}
-// Store weather information to local storage
-function storeWeatherInfo() {}
-// Show search history
+  iconVal = localStorage.getItem("Icon");
+} */
+
+/* Display previously search cities and make call to show data*/
 function displaySearchHistory() {
   for (var i = 0; i < currentCityArray.length; i++) {
     var searchCity = currentCityArray[i];
@@ -135,21 +128,30 @@ function displaySearchHistory() {
         `).appendTo("#searchHistory");
   }
 }
-//searchCity();
-//searchBtn.addEventListener("click", searchCity);
+
 searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
   searchCity();
-  document.getElementById("search-field").value = " ";
+  $("#search-field").val("");
   displaySearchHistory();
   // clear div of previous five day search
   $("#forecast-div").empty();
 });
+
+// This function clears local storage
 clearButton.addEventListener("click", function () {
   localStorage.clear();
   searchHistory.style.display = "none";
+  // currentCityArray.splice(0, currentCityArray.length);
 });
-searchedCity.addEventListener("click", function (e) {
-  e.preventDefault();
-  searchCity();
+
+// This function recalls the 2 forecast functions
+$(searchHistory).click(function (e) {
+  console.log("button clicked");
+  var buttonLabel = e.target.textContent;
+  var newInput = buttonLabel.trim();
+  console.log(newInput);
+  getCityWeatherInfo(newInput);
+  getForecast(newInput);
+  $("#forecast-div").empty();
 });
--- myREADME.md
+++ myREADME.md
@@ -1,2 +1,6 @@
 # weather-dashboard
 Weather dashboard that runs in the browser and features dynamically updated HTML and CSS.
+
+This application still has a few bugs that need to be worked through to satisfy the user experience. For example the UV index has not been incorporated. Another shortfall, is that when a searched city is added, all previous searches are looped through and added to the page causing a repeat * the number of items that have been searched. I believe that that the local storage is persistent, but I need more feedback in this area. I feel that the layout is responsive to for small medium and large screen sizes.
+
+
